733988cc73b58cb708b744b8d10da70ba5965fe9
5hubham5ingh <shubhamanitasingh@gmail.com>
2024-04-29 03:41:15 +0530

    added refresh capability to dashboard script

--- .scripts/weatherAndSysInfo.sh
+++ .scripts/weatherAndSysInfo.sh
@@ -3,6 +3,7 @@
 curl -s wttr.in | sed -n '8,17p'
 
 while true; do
+
     # Store the output of upower command in an array
     readarray -t batteryInfoArray <<< "$(upower -i /org/freedesktop/UPower/devices/battery_BAT1 | sed -n -E '/state|percentage/{s/^\s+|\s+$//g;p}')"
 
@@ -22,4 +23,15 @@ while true; do
     printf "%-25s\n" "|--------------------------------------|-----------------------------------|"
 
     sleep 300
+
+    # Move the cursor up 5 lines
+    printf "\033[6A"
+
+    # Clear the next 5 lines
+    for ((i = 0; i < 6; i++)); do
+        printf "\033[K\n"
+    done
+
+     # Move the cursor up 5 lines
+    printf "\033[6A"
 done
