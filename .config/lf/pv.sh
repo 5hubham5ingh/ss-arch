f6122fecc1f81e8af5d04b21ef73730d9384996e
Shubham Singh <128460722+5hubham5ingh@users.noreply.github.com>
2024-04-29 11:46:48 +0530

    kitty image viewer

--- .config/lf/pv.sh
+++ .config/lf/pv.sh
@@ -10,9 +10,12 @@ case "$1" in
     *.rar) unrar l "$1";;
     *.7z) 7z l "$1";;
     *.pdf) less "$1";;
-    *.png) kitty icat --place "${w}x${h}@${x}x${y}" "$1" ;;
-    *.jpg)  kitty icat --place "${w}x${h}@${x}x${y}" "$1" ;;
-    *.jpeg) kitty icat --place "${w}x${h}@${x}x${y}" "$1" ;;
+    *.png) kitty +kitten icat --silent --stdin no --transfer-mode file --place "${w}x${h}@${x}x${y}" "$1" < /dev/null > /dev/tty
+} ;;
+    *.jpg) kitty +kitten icat --silent --stdin no --transfer-mode file --place "${w}x${h}@${x}x${y}" "$1" < /dev/null > /dev/tty
+} ;;
+    *.jpeg) kitty +kitten icat --silent --stdin no --transfer-mode file --place "${w}x${h}@${x}x${y}" "$1" < /dev/null > /dev/tty
+} ;;
     *)  unset COLORTERM; bat --wrap=auto --color=always --style=full "$1";;
 esac
 
